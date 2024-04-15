03628b9af2393f9275554ceb4a40622679347ae9
Belmaxi <88465067+Belmaxi@users.noreply.github.com>
2024-04-15 21:10:43 +0800

    Update mysql.go

--- pkg/config/mysql.go
+++ pkg/config/mysql.go
@@ -9,7 +9,7 @@ var DB *sql.DB
 
 func init() {
 	var err error
-	DB, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/counseling_statistics?charset=utf8&parseTime=True")
+	DB, err = sql.Open("mysql", "root:5-07pEf5xYW5@tcp(localhost:3306)/counseling_statistics?charset=utf8&parseTime=True")
 	if err != nil {
 		panic(err)
 	}
